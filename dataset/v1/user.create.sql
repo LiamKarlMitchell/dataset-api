0bf8f44eb191a7a516f9f819f1db702a90877185
Unknown <p.d.walczak@gmail.com>
2018-03-28 13:38:30 +0200

    Cleaning up repo

--- dataset/v1/user.create.sql
+++
@@ -1,8 +0,0 @@
--- To use ${form.anything}
--- To use ${param.anything}
--- To use ${query.anything}
-
--- anrinch.com/api/user/:`param`?`query`=one
--- body sent in `form`
-
-SELECT * FROM ${form.database} WHERE test = ${params.test}
